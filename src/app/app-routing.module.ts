d930e39348eaebddfb89f2da087690a3dd8c71e3
Higor de Deus <higordeus22@gmail.com>
2023-10-30 16:51:25 -0300

    LOGO OK. TEMA IFOOD OK. FONTES OK. DOCKER OK.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
+// src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./components/modules/admin/admin.module').then(
+        (m) => m.AdminModule
+      ),
+  },
+  {
+    path: 'client',
+    loadChildren: () =>
+      import('./components/modules/client/client.module').then(
+        (m) => m.ClientModule
+      ),
+  },
+  { path: '', redirectTo: '/client', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'GastroSyncFrontend';
+  themeClass = 'ifood-light-theme';
+
+  constructor(private overlayContainer: OverlayContainer) {
+    this.setTheme(this.themeClass);
+  }
+
+  setTheme(theme: string) {
+    this.themeClass = theme;
+    const classList = this.overlayContainer.getContainerElement().classList;
+    classList.remove('ifood-light-theme', 'ifood-dark-theme');
+    classList.add(theme);
+  }
+
+  toggleTheme() {
+    this.setTheme(
+      this.themeClass === 'ifood-light-theme'
+        ? 'ifood-dark-theme'
+        : 'ifood-light-theme'
+    );
+  }
 }
