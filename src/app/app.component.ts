feb6faecccdafd6e1e31396927bff6628be99853
Higor de Deus <higordeus22@gmail.com>
2023-10-31 13:53:12 -0300

    Paleta de cores e fonte do ifood implementadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { ThemeService } from './components/services/theme.service';
 import { ClientOptionsPanelService } from './components/services/OptionsPanelService';
 
@@ -13,15 +13,35 @@ export class AppComponent {
 
   constructor(
     private themeService: ThemeService,
-    private clientOptionsPanelService: ClientOptionsPanelService
-  ) {}
+    private clientOptionsPanelService: ClientOptionsPanelService,
+    private renderer: Renderer2 // Injeção do Renderer2
+  ) {
+    // Definir o tema claro como padrão ao iniciar
+    this.setTheme('ifood-light-theme');
+  }
 
   toggleTheme(): void {
     this.themeService.toggleTheme();
     this.themeIcon = this.themeService.isDarkTheme() ? 'moon' : 'sun';
+
+    // Alternar entre os temas baseado no status do tema
+    const themeClass = this.themeService.isDarkTheme()
+      ? 'ifood-dark-theme'
+      : 'ifood-light-theme';
+    this.setTheme(themeClass);
   }
 
   openClientOptions(): void {
     this.clientOptionsPanelService.openClientOptions();
+  }
+
+  private setTheme(themeClass: string): void {
+    if (themeClass === 'ifood-dark-theme') {
+      this.renderer.removeClass(document.body, 'ifood-light-theme');
+      this.renderer.addClass(document.body, 'ifood-dark-theme');
+    } else {
+      this.renderer.removeClass(document.body, 'ifood-dark-theme');
+      this.renderer.addClass(document.body, 'ifood-light-theme');
+    }
   }
 }
