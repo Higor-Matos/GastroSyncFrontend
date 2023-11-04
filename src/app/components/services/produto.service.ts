a5f9284c34c8b8e8b10f69bcd9515b67cf6004a6
Higor de Deus <higordeus22@gmail.com>
2023-11-03 21:56:31 -0300

    organização de código

--- src/app/components/services/produto.service.ts
+++ src/app/components/services/produto.service.ts
@@ -7,10 +7,24 @@ import { Observable } from 'rxjs';
 })
 export class ProdutoService {
   private url = 'https://localhost:8443/api/Produto/RecuperarTodosProdutos';
+  private imageMap: { [key: number]: string } = {
+    1: 'https://i.ibb.co/gW8BHP7/pizza.jpg',
+    2: 'https://i.ibb.co/92yvPZF/Sushi.jpg',
+    3: 'https://i.ibb.co/5n4J9YK/Macarr-o.jpg',
+    4: 'https://i.ibb.co/qpk5ByM/Hamburger.jpg',
+    5: 'https://i.ibb.co/gdqg4K5/Suco.jpg',
+    6: 'https://i.ibb.co/KKV9GtC/gua.jpg',
+    7: 'https://i.ibb.co/kxwwz3Z/Refrigerante.jpg',
+    8: 'https://i.ibb.co/WzJn0br/Cerveja.jpg',
+  };
 
   constructor(private http: HttpClient) {}
 
   getProdutos(): Observable<any> {
     return this.http.get(this.url);
+  }
+
+  getImageUrl(id: number): string {
+    return this.imageMap[id] || 'caminho_para_imagem_padrao.jpg';
   }
 }
--- src/app/components/shared/cardapio/cardapio.component.ts
+++ src/app/components/shared/cardapio/cardapio.component.ts
@@ -12,6 +12,13 @@ interface Produto {
   nome: string;
   categoria: string;
   preco: number;
+  imageUrl?: string; // Opcional, caso você decida gerar a URL aqui
+}
+
+interface Categoria {
+  nome: string;
+  icon: string;
+  produtos: Produto[];
 }
 
 @Component({
@@ -20,37 +27,51 @@ interface Produto {
   styleUrls: ['./cardapio.component.scss'],
 })
 export class CardapioComponent implements OnInit {
-  produtos: Produto[] = [];
-  comidas: Produto[] = [];
-  bebidas: Produto[] = [];
+  categorias: Categoria[] = [];
 
   constructor(private produtoService: ProdutoService) {}
 
-  ngOnInit() {
-    this.produtoService.getProdutos().subscribe((response: ProdutoResponse) => {
-      if (response.success) {
-        this.produtos = response.data;
-        this.comidas = this.produtos.filter(
-          (produto) => produto.categoria === 'Comida'
-        );
-        this.bebidas = this.produtos.filter(
-          (produto) => produto.categoria === 'Bebida'
-        );
-      }
+  ngOnInit(): void {
+    this.produtoService.getProdutos().subscribe({
+      next: (resposta: ProdutoResponse) => {
+        if (resposta.success && Array.isArray(resposta.data)) {
+          this.categorias = this.organizarCategorias(resposta.data);
+        } else {
+          console.error(
+            'A resposta recebida não contém um array válido:',
+            resposta
+          );
+        }
+      },
+      error: (err) =>
+        console.error('Ocorreu um erro ao buscar os produtos:', err),
     });
   }
 
-  getImageUrl(id: number): string {
-    const imageMap: { [key: number]: string } = {
-      1: 'https://i.ibb.co/gW8BHP7/pizza.jpg',
-      2: 'https://i.ibb.co/92yvPZF/Sushi.jpg',
-      3: 'https://i.ibb.co/5n4J9YK/Macarr-o.jpg',
-      4: 'https://i.ibb.co/qpk5ByM/Hamburger.jpg',
-      5: 'https://i.ibb.co/gdqg4K5/Suco.jpg',
-      6: 'https://i.ibb.co/KKV9GtC/gua.jpg',
-      7: 'https://i.ibb.co/kxwwz3Z/Refrigerante.jpg',
-      8: 'https://i.ibb.co/WzJn0br/Cerveja.jpg',
+  private organizarCategorias(produtos: Produto[]): Categoria[] {
+    const categoriasMap = new Map<string, Categoria>();
+    produtos.forEach((produto) => {
+      if (!categoriasMap.has(produto.categoria)) {
+        categoriasMap.set(produto.categoria, {
+          nome: produto.categoria,
+          icon: this.getCategoriaIcon(produto.categoria),
+          produtos: [],
+        });
+      }
+      categoriasMap.get(produto.categoria)?.produtos.push({
+        ...produto,
+        imageUrl: this.produtoService.getImageUrl(produto.id), // Garanta que esta função exista e retorne a URL
+      });
+    });
+    return Array.from(categoriasMap.values());
+  }
+
+  private getCategoriaIcon(categoria: string): string {
+    const icons: { [key: string]: string } = {
+      Comida: 'restaurant_menu',
+      Bebida: 'local_cafe',
+      // Adicionar mais categorias e ícones conforme necessário
     };
-    return imageMap[id] || 'caminho_para_imagem_padrao.jpg';
+    return icons[categoria] || 'default_icon';
   }
 }
