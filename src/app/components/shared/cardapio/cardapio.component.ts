47854642b0f0deec67af3a9b80ee0676c836cb05
Higor de Deus <higordeus22@gmail.com>
2023-11-03 21:07:57 -0300

    agora imagens aparecem

--- src/app/components/shared/cardapio/cardapio.component.ts
+++ src/app/components/shared/cardapio/cardapio.component.ts
@@ -31,4 +31,18 @@ export class CardapioComponent implements OnInit {
       }
     });
   }
+
+  getImageUrl(id: number): string {
+    const imageMap: { [key: number]: string } = {
+      1: 'https://i.ibb.co/gW8BHP7/pizza.jpg',
+      2: 'https://i.ibb.co/92yvPZF/Sushi.jpg',
+      3: 'https://i.ibb.co/5n4J9YK/Macarr-o.jpg',
+      4: 'https://i.ibb.co/qpk5ByM/Hamburger.jpg',
+      5: 'https://i.ibb.co/gdqg4K5/Suco.jpg',
+      6: 'https://i.ibb.co/KKV9GtC/gua.jpg',
+      7: 'https://i.ibb.co/kxwwz3Z/Refrigerante.jpg',
+      8: 'https://i.ibb.co/WzJn0br/Cerveja.jpg',
+    };
+    return imageMap[id] || 'caminho_para_imagem_padrao.jpg';
+  }
 }
