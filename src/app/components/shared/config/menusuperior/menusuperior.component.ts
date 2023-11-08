148cde284c0868bbc429a0dc9e25468c238f104c
Higor de Deus <higordeus22@gmail.com>
2023-11-07 23:26:02 -0300

    organização de código ok

--- src/app/components/shared/config/menusuperior/menusuperior.component.ts
+++ src/app/components/shared/config/menusuperior/menusuperior.component.ts
@@ -1,7 +1,10 @@
+// menusuperior.component.ts
+
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ThemeService, ThemeType } from '../../../services/tema/theme.service';
 import { MesaService } from '../../../services/mesa/mesa.service';
 import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-menu-superior',
   templateUrl: './menusuperior.component.html',
@@ -10,8 +13,8 @@ import { Subscription } from 'rxjs';
 export class MenuSuperiorComponent implements OnInit, OnDestroy {
   iconeTema = 'wb_sunny';
   numeroMesa: number | null = null;
-  temaClaro = true; // Inicialmente definido como tema claro
-  private mesaSubscription!: Subscription;
+  temaClaro = true;
+  private subscriptions = new Subscription();
 
   constructor(
     private themeService: ThemeService,
@@ -19,40 +22,31 @@ export class MenuSuperiorComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.mesaSubscription = this.mesaService.numeroDaMesaAtual$.subscribe(
-      (numeroMesa: number | null) => {
-        this.numeroMesa = numeroMesa;
-      }
+    this.subscriptions.add(
+      this.mesaService.numeroDaMesaAtual$.subscribe(
+        (numeroMesa) => (this.numeroMesa = numeroMesa)
+      )
     );
+
+    this.subscriptions.add(
+      this.themeService.themeChanged.subscribe((theme) => {
+        this.temaClaro = theme === ThemeType.Light;
+        this.iconeTema = this.temaClaro ? 'wb_sunny' : 'brightness_2';
+      })
+    );
+
     this.obterNumeroMesa();
-    this.atualizarEstadoTema();
-    this.themeService.themeChanged.subscribe((theme: ThemeType) => {
-      this.temaClaro = theme === ThemeType.Light;
-      this.atualizarIconeTema();
-    });
   }
 
   ngOnDestroy(): void {
-    this.mesaSubscription.unsubscribe();
+    this.subscriptions.unsubscribe();
   }
 
   alternarTema(): void {
     this.themeService.toggleTheme();
   }
 
-  private atualizarIconeTema(): void {
-    this.iconeTema = this.themeService.isDarkTheme()
-      ? 'brightness_2'
-      : 'wb_sunny';
-  }
-
   obterNumeroMesa(): void {
     this.numeroMesa = this.mesaService.obterNumeroDaMesa();
-  }
-
-  private atualizarEstadoTema(): void {
-    this.temaClaro =
-      this.themeService.getCurrentThemeClass() === ThemeType.Light;
-    this.atualizarIconeTema();
   }
 }
