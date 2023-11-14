9a24f8a08abe0bd3e6bf5d29ae6cd9b144792ffd
Higor de Deus <higordeus22@gmail.com>
2023-11-14 02:31:16 -0300

    book inserido

--- src/app/components/modules/client/module/client.module.ts
+++ src/app/components/modules/client/module/client.module.ts
@@ -7,10 +7,16 @@ import { SharedModule } from '../../../shared/module/shared.module';
 import { PagamentosComponent } from '../pagamentos/pagamentos.component';
 import { MatCardModule } from '@angular/material/card';
 import { TotalCardComponent } from '../pagamentos/totalcard/totalcard.component';
+import { BookComponent } from '../pagamentos/book/book.component';
 
 @NgModule({
-  declarations: [ClientComponent, PagamentosComponent, TotalCardComponent],
+  declarations: [
+    ClientComponent,
+    PagamentosComponent,
+    TotalCardComponent,
+    BookComponent,
+  ],
   imports: [CommonModule, ClientRoutingModule, SharedModule, MatCardModule],
-  exports: [TotalCardComponent],
+  exports: [TotalCardComponent, BookComponent],
 })
 export class ClientModule {}
---
+++ src/app/components/modules/client/pagamentos/book/book.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidos',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss'],
+})
+export class BookComponent {}
