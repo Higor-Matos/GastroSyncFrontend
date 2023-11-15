cde3551e42b9737b94cdd4ee460e110bd9d17c7e
Higor de Deus <higordeus22@gmail.com>
2023-11-15 02:13:25 -0300

    componente ruim instalado

--- src/app/components/modules/client/module/client.module.ts
+++ src/app/components/modules/client/module/client.module.ts
@@ -7,10 +7,16 @@ import { SharedModule } from '../../../shared/module/shared.module';
 import { PagamentosComponent } from '../pagamentos/pagamentos.component';
 import { MatCardModule } from '@angular/material/card';
 import { TotalCardComponent } from '../pagamentos/totalcard/totalcard.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [ClientComponent, PagamentosComponent, TotalCardComponent],
-  imports: [CommonModule, ClientRoutingModule, SharedModule, MatCardModule],
-  exports: [TotalCardComponent],
+  imports: [
+    CommonModule,
+    ClientRoutingModule,
+    SharedModule,
+    MatCardModule,
+    NgxChartsModule,
+  ],
 })
 export class ClientModule {}
