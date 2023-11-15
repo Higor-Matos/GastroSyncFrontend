cc0ddd1f01f5e76f50831527d2e907b8ad9d5d91
Higor de Deus <higordeus22@gmail.com>
2023-11-14 22:49:33 -0300

    cards consumidor.

--- src/app/components/modules/client/module/client.module.ts
+++ src/app/components/modules/client/module/client.module.ts
@@ -7,16 +7,10 @@ import { SharedModule } from '../../../shared/module/shared.module';
 import { PagamentosComponent } from '../pagamentos/pagamentos.component';
 import { MatCardModule } from '@angular/material/card';
 import { TotalCardComponent } from '../pagamentos/totalcard/totalcard.component';
-import { BookComponent } from '../pagamentos/book/book.component';
 
 @NgModule({
-  declarations: [
-    ClientComponent,
-    PagamentosComponent,
-    TotalCardComponent,
-    BookComponent,
-  ],
+  declarations: [ClientComponent, PagamentosComponent, TotalCardComponent],
   imports: [CommonModule, ClientRoutingModule, SharedModule, MatCardModule],
-  exports: [TotalCardComponent, BookComponent],
+  exports: [TotalCardComponent],
 })
 export class ClientModule {}
